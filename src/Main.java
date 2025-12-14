5f07aaf21be4508834bc73cec7eb3a39307f6676
Arslan Ismailov <ismailov390@yandex.ru>
2025-12-15 01:51:10 +0300

    Added methods sort(Сomparator<? super T> c)

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,71 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        SuperArrayList<String> list = new SuperArrayList<>();
-//        list.add(0, "A");
-//        list.add(1, "B");
-//        list.add(2, "C");
-//        list.add(3, "D");
-//        System.out.println(list);
-//        list.clear();
-//        System.out.println(list);
-//        List<String> list2 = new ArrayList<>();
-//        list2.add(0, "А");
-//        list2.add(1, "Б");
-//        list2.add(2, "В");
-//        System.out.println(list2);
-//        list.addAll(list2);
-//        System.out.println(list);
-//        list.add(1, "AR");
-//        System.out.println(list);
-//        list.add(8, "I");
-//        list.add(9, "J");
-//        System.out.println(list);
-//        list.addAll(list2);
-//        System.out.println(list);
-//        list.clear();
-//        System.out.println(list);
-//        System.out.println(list2);
+
+        // метод add(int i, T element)
+        SuperArrayList<Integer> list = new SuperArrayList<>();
+        list.add(0, 11);
+        list.add(1, 9);
+        list.add(2, 18);
+        list.add(3, 31);
+        list.add(4, 52);
+        list.add(5, 113);
+        list.add(6, 4);
+        list.add(7, 1);
+        list.add(8, 1);
+        list.add(9, 73);
+        list.add(10, 18);
+        System.out.println(list);
+
+        // метод addAll(Collection<? extends T> c)
+        SuperArrayList<Integer> list2 = new SuperArrayList<>();
+        list2.add(0, 1);
+        list2.add(1, 115);
+        list2.add(2, 48);
+        list2.add(3, 31);
+        list2.add(4, 22);
+        System.out.println(list2);
+
+        List<Integer> arrayList = Arrays.asList(1, 77, 38, 41);
+        list2.addAll(arrayList);
+        System.out.println(list2);
+
+        list.addAll(list2);
+        System.out.println(list);
+
+        // метод clear()
+        list2.clear();
+        System.out.println(list2);
+
+        // метод  get(int i)
+        System.out.println(list.get(2));
+
+        // метод isEmpty()
+        System.out.println(list2.isEmpty());
+        System.out.println(list.isEmpty());
+
+        // метод remove(int i) remove(Object o)
+        System.out.println(list.size());
+        list.remove(2);
+        System.out.println(list.get(2));
+        list.remove((Integer) 115);
+        System.out.println(list.size());
+        System.out.println(list);
+
+        // метод sort(Сomparator<? super T> c)
+
+        list.sort(Integer::compareTo);
+        System.out.println(list);
+        list.sort(Collections.reverseOrder());
+        System.out.println(list);
+
+        SuperArrayList<Integer> list3 = new SuperArrayList<>(arrayList);
+        System.out.println(list3);
+
+
 
     }
 }
--- src/SuperArrayList.java
+++ src/SuperArrayList.java
@@ -1,55 +1,42 @@
-import java.util.Collection;
+import java.util.*;
 
-public class SuperArrayList<V> {
-
+public class SuperArrayList<V> implements List<V> {
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] array;
     private int size;
     private int capacity;
 
-    public SuperArrayList() {
-        this.capacity = 10;
-        this.array = new Object[capacity];
-    }
-
-    public SuperArrayList(int initialCapacity) {
-        if (initialCapacity > 0) {
-            this.capacity = initialCapacity;
-        } else if (initialCapacity == 0) {
-            this.capacity = 10;
+    public SuperArrayList(int capacity) {
+        if (capacity > 0) {
+            this.capacity = capacity;
+            this.array = new Object[capacity];
+        } else if (capacity == 0) {
+            this.capacity = DEFAULT_CAPACITY;
+            this.array = new Object[capacity];
         } else {
-            throw new IllegalArgumentException("Неверная вместимость: " + initialCapacity);
+            throw new IllegalArgumentException("Некорректная вместимость: " + capacity);
         }
+    }
+
+    public SuperArrayList() {
+        this.capacity = DEFAULT_CAPACITY;
         this.array = new Object[capacity];
     }
 
-
-    public void add(int index, V element) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", size: " + size);
-        }
-        if (size == array.length) array = grow();
-        System.arraycopy(array, index, array, index + 1, size - index);
-        array[index] = element;
-        size++;
-    }
-
-    public boolean addAll(Collection<? extends V> collection) {
+    public SuperArrayList(Collection<? extends V> collection) {
         Object[] elements = collection.toArray();
-        if (elements.length == 0) return false;
-        if (elements.length > capacity - size) {
-            int requiredCapacity = elements.length + size;
-            array = grow(requiredCapacity);
+        if ((size = elements.length) != 0) {
+            if (collection.getClass() == SuperArrayList.class) {
+                array = elements;
+            } else {
+                array = Arrays.copyOf(elements, size, Object[].class);
+            }
+        } else {
+            this.capacity = DEFAULT_CAPACITY;
+            this.array = new Object[capacity];
         }
-        System.arraycopy(elements, 0, array, size, elements.length);
-        size += elements.length;
-        return true;
     }
 
-    public void clear() {
-        capacity = 10;
-        array = new Object[capacity];
-        size = 0;
-    }
 
     public Object[] grow(int requiredCapacity) {
         while (requiredCapacity > capacity) {
@@ -67,6 +54,109 @@ public class SuperArrayList<V> {
     }
 
     @Override
+    public void add(int index, V element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", size: " + size);
+        }
+        if (size == array.length) array = grow();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = element;
+        size++;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends V> collection) {
+        Object[] elements = collection.toArray();
+        if (elements.length == 0) return false;
+        if (elements.length > capacity - size) {
+            int requiredCapacity = elements.length + size;
+            array = grow(requiredCapacity);
+        }
+        System.arraycopy(elements, 0, array, size, elements.length);
+        size += elements.length;
+        return true;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(array, size);
+    }
+
+    @Override
+    public void clear() {
+        capacity = 10;
+        array = new Object[capacity];
+        size = 0;
+    }
+
+    @Override
+    public V get(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", size: " + size);
+        return (V) array[index];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public V remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", size: " + size);
+        V oldValue = (V) array[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(array, index + 1, array, index, numMoved);
+        }
+        array[--size] = null;
+        return oldValue;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(o)) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void sort(Comparator<? super V> comparator) {
+        int leftIndex = 0;
+        int rightIndex = size - 1;
+        quickSort(comparator, leftIndex, rightIndex);
+    }
+
+    private void quickSort(Comparator<? super V> comparator, int start, int end) {
+        if (start >= end || array.length == 0) return;
+        V pivot = (V) array[(start + end) / 2];
+        int startMarker = start, endMarker = end;
+        while (startMarker <= endMarker) {
+            while (comparator.compare((V) array[startMarker], pivot) < 0) startMarker++;
+            while (comparator.compare((V) array[endMarker], pivot) > 0) endMarker--;
+            if (startMarker <= endMarker) {
+                V temp = (V) array[startMarker];
+                array[startMarker] = array[endMarker];
+                array[endMarker] = temp;
+                startMarker++;
+                endMarker--;
+            }
+        }
+        if (start < endMarker) quickSort(comparator, start, endMarker);
+        if (startMarker < end) quickSort(comparator, startMarker, end);
+    }
+
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
@@ -76,5 +166,80 @@ public class SuperArrayList<V> {
         }
         sb.append("]");
         return sb.toString();
+    }
+
+
+    /**
+     * Методы которые еще не реализованны
+     */
+
+    @Override
+    public boolean add(V v) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends V> c) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public V set(int index, V element) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public ListIterator<V> listIterator() {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public ListIterator<V> listIterator(int index) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
+    }
+
+    @Override
+    public List<V> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Будет доступно в следующей версии.");
     }
 }
